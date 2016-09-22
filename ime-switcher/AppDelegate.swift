5d0700e0dd08f177c55df14764809e455e1d275a
Kan Fushihara <kan.fushihara@gmail.com>
2016-09-22 10:26:07 +0900

    Initial Commit

---
+++ ime-switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ime-switcher
+//
+//  Created by FushiharaKan on 2016/09/22.
+//  Copyright © 2016年 Kan Fushihara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ime-switcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ime-switcher
+//
+//  Created by FushiharaKan on 2016/09/22.
+//  Copyright © 2016年 Kan Fushihara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
